3fca0895948661b8d28dcc0872d60fd256c07d63
alice <castaldialicee@gmail.com>
2024-02-02 20:39:16 +0100

    Aggiungere i file di progetto.

---
+++ Frazione/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione frazione = new Frazione(4,6);
+            Console.WriteLine(frazione.ToString());
+            Console.ReadLine();
+        }
+    }
+}
